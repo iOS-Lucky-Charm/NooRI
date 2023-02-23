4914ea88deaf7f1d56d9306e99c32994b54e9a71
CHO_noori <93557885+loveitall@users.noreply.github.com>
2023-02-23 17:41:27 +0900

    [Feat] #1-세팅
    git 브랜치 생성하다가 파일 날아가서..다시 폴더링하고 작업하겠습니다..!

---
+++ iOS_Kakao/iOS_Kakao/Auth/WelcomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeVC.swift
+//  iOS_Kakao
+//
+//  Created by 누리링 on 2023/02/23.
+//
+
+import UIKit
+import Then
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
